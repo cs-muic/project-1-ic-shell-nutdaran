c916cac7b46cf865de4dd165e91712727721e0aa
nutdaran <daran.thw@student.mahidol.edu>
2023-09-22 21:06:53 +0700

    1st milestone

--- icsh.c
+++ icsh.c
@@ -1,17 +1,107 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name: Daran Thawornwattanapol
+ * StudentID: 6380413
  */
 
 #include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
 #define MAX_CMD_BUFFER 255
 
-int main() {
+// Echo's helper function
+void printString(char *args[])
+{
+    for (int i = 1; args[i] != NULL; i++)
+    {
+        printf("%s", args[i]);
+        if (args[i + 1] != NULL)
+        {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
     char buffer[MAX_CMD_BUFFER];
-    while (1) {
+    char *args[MAX_CMD_BUFFER / 2];
+    char lastCmd[MAX_CMD_BUFFER] = "";
+
+    while (1)
+    {
         printf("icsh $ ");
         fgets(buffer, 255, stdin);
-        printf("you said: %s\n", buffer);
+
+        // When users give an empty command, your shell just give a new prompt.
+        if (strlen(buffer) == 1)
+        {
+            continue;
+        }
+        else
+        {
+            // Remove '\n'
+            buffer[strlen(buffer) - 1] = '\0';
+
+            // Double-bang: repeat the last command given to the shell. 
+            if (strcmp(buffer, "!!") == 0)
+            {
+                if (strlen(lastCmd) == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    // Replace the buffer with lastCmd and run it
+                    strcpy(buffer,lastCmd); 
+                    printf("%s\n", buffer);
+                }
+            }
+            else {
+                // store the buffer into the lastCmd
+                strcpy(lastCmd, buffer);
+            }
+
+            // Tokenize the buffer
+            char *token = strtok(buffer, " ");
+            int i = 0;
+            while (token != NULL)
+            {
+                args[i] = token;
+                token = strtok(NULL, " ");
+                i++;
+            }
+            args[i] = NULL;
+            char *cmd = args[0];
+
+            // echo <text> -- the echo command prints a given text 
+            // (until EOL) back to the console.
+            if (strcmp(cmd, "echo") == 0)
+            {
+                printString(args);
+            }
+            // exit <num> -- this command exits the shell with a given exit code.
+            else if (strcmp(cmd, "exit") == 0)
+            {
+                int exitCode = atoi(args[1]);
+                if (exitCode >= 0 && exitCode <= 255)
+                {
+                    printf("Good bye!\n");
+                    exit(exitCode);
+                }
+                // if the exit code is more than 255, truncate into 8 bit
+                else
+                {
+                    exitCode = exitCode >> 8;
+                    printf("Good bye!\n");
+                    exit(exitCode);
+                }
+            }
+            else
+            {
+                printf("bad command\n");
+            }
+        }
     }
 }
